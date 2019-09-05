3f4e639b787a92e02064984a420dd300290d3901
Simona <simonaeneva@yahoo.com>
2019-09-05 21:05:01 +0300

    Refactored update and getAll methods

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -4,19 +4,16 @@ import { CreateTaskDto } from './dto/create-task-dto'
 import { GetTasksFilterDto } from './dto/get-tasks-filter-dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task } from './task.entity';
+import { TaskStatus } from "./task-status.enum";
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService){}
 
-    // @Get()
-    // getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[]{
-    //     if (Object.keys(filterDto).length) {
-    //         return this.tasksService.getTasksWithFilters(filterDto)
-    //     } else {
-    //         return this.tasksService.getAllTasks()
-    //     }
-    // }
+    @Get()
+    getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Promise<Task[]>{
+        return this.tasksService.getTasks(filterDto)
+    }
 
     @Get(':id')
     getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
@@ -34,11 +31,11 @@ export class TasksController {
         return this.tasksService.createTask(createtaskDto)        
     }
 
-    // @Patch(':id/status')
-    // updateTaskStatus(
-    //     @Param('id') id: string,
-    //     @Body('status', TaskStatusValidationPipe) status: TaskStatus
-    // ): Task {
-    //     return this.tasksService.updateTaskStatus(id, status)
-    // }
+    @Patch(':id/status')
+    updateTaskStatus(
+        @Param('id', ParseIntPipe) id: number,
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus
+    ): Promise<Task> {
+        return this.tasksService.updateTaskStatus(id, status)
+    }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -18,28 +18,9 @@ export class TasksService {
 
 
 
-    // getAllTasks(): Task[] {
-    //     return this.tasks
-    // }
-
-    // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    //     const {status, search} = filterDto
-
-    //     let tasks = this.getAllTasks()
-
-    //     if(status) {
-    //         tasks = tasks.filter(t => t.status === status)
-    //     }
-
-    //     if(search) {
-    //         tasks = tasks.filter(t => 
-    //             t.title.includes(search) || 
-    //             t.description.includes(search)
-    //             )
-    //     }
-
-    //     return tasks
-    // }
+    getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+        return this.taskRepository.getTasks(filterDto)
+    }
 
     async getTaskById(id: number): Promise<Task> {
         const found = await this.taskRepository.findOne(id)
@@ -66,7 +47,12 @@ export class TasksService {
         
      }
 
-    // updateTaskStatus(id: number, status: TaskStatus): Promise<UpdateResult> {
-    //     return this.taskRepository.update(id,status)
-    // }
+    async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+        const task = await this.getTaskById(id)
+        task.status = status
+
+        await task.save()
+
+        return task
+    }
 }
