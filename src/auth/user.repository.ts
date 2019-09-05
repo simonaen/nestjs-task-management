29f2d620a2ab71338fd7a3f90afbd6343c1212d6
Simona <simonaeneva@yahoo.com>
2019-09-05 22:21:11 +0300

    Added user entity and authentication module

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,26 @@
+import { Repository, EntityRepository } from "typeorm";
+import { User } from "./user.entity";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { Task } from "src/tasks/task.entity";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+    async signUp (authCredDto: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCredDto
+        
+        const user = new User()
+        user.username = username
+        user.password = password
+
+        try{
+            await user.save()
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('Username already exists')
+            } else {
+                throw new InternalServerErrorException()
+            }
+        }
+    }
+}
