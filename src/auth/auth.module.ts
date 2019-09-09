2ded64adbf185f055136d4ff77787266d561e43b
Simona <simonaeneva@yahoo.com>
2019-09-09 16:09:41 +0300

    Added configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserRepository } from './user.repository';
+import { Module } from '@nestjs/common'
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UserRepository } from './user.repository'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
-import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './jwt.strategy'
+import * as config from 'config'
+
+const jwtConfig = config.get('jwt')
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserRepository]),
     JwtModule.register({
-      secret: 'topSecret12',
+      secret: process.env.JWT_SECRET || jwtConfig.secret,
       signOptions: {
-        expiresIn: 3600,
+        expiresIn: jwtConfig.expiresIn,
       },
     }),
     PassportModule.register({
